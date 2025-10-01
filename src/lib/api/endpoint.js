30b3f010988933d53570f970f9271ae1b24b7c09
Almadhani <almadhani17@students.amikom.ac.id>
2025-10-01 15:13:20 +0700

    feat(endpoint): added new endpoint to get and post about files by folderId

--- src/lib/api/endpoint.js
+++ src/lib/api/endpoint.js
@@ -6,12 +6,15 @@ export const DOCUMENT_SERVICE = {
   GET: {
     getAllUsers: "/user/all",
     getAllFolders: "/folder/all",
+    getAllFilesByFolderId: (id) => `/folder/${id}`,
   },
   POST: {
     loginUser: "/auth/login",
     registerUser: "auth/register",
     logoutUser: "/auth/logout",
     createFolder: "/folder/add",
+    createFileBulk: (folderParentId) =>
+      `folder/${folderParentId}/add-file/bulk`,
   },
   PUT: {
     updateFolder: (id) => `/folder/${id}`,
