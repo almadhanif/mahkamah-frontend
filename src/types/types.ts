9556dec1e7aa69394cc0cf5b26c987bff62071db
Almadhani <almadhani17@students.amikom.ac.id>
2025-10-01 15:14:56 +0700

    feat(types): initialize types from new endpoint

--- src/types/types.ts
+++ src/types/types.ts
@@ -23,3 +23,36 @@ export interface User {
   email: string;
   role_code: string;
 }
+
+export type Folder = {
+  id: number;
+  name: string;
+  parent_id: number | null;
+  children?: Folder[];
+  deletedAt?: string | null;
+  createdAt?: string;
+  updatedAt?: string | null;
+};
+
+export type File = {
+  file_id: number;
+  name: string;
+  file_path: string;
+  file_size: number;
+  folder_id: number;
+  file_type: string;
+  createdAt: string;
+  updatedAt: string | null;
+  deletedAt: string | null;
+};
+
+export interface FileResponse {
+  success: boolean;
+  folder: Folder;
+  content: {
+    subfolders: Folder[];
+    files: File[];
+  };
+  message: string;
+  code: number;
+}
